713ecbfd7bdd4744922a76c0b0aa74b2e6d70ace
laithalkinani <alkinanl@uwindsor.ca>
2025-07-23 23:04:22 -0700

    Added arduino code for CAN transmission

---
+++ ARDUINO-MCP2515/arduino_mcp2515/arduino_mcp2515.ino
@@ -0,0 +1,31 @@
+#include <CAN.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  Serial.println("CAN Sender");
+
+  // start the CAN bus at 500 kbps
+  if (!CAN.begin(500E3)) {
+    Serial.println("Starting CAN failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  // send packet: id is 11 bits, packet can contain up to 8 bytes of data
+  Serial.print("Sending packet ... ");
+
+  CAN.beginPacket(0x12); //id of the msg is arbitrary, but lower id's have higher priority on the bus
+  CAN.write('h');
+  CAN.write('e');
+  CAN.write('l');
+  CAN.write('l');
+  CAN.write('o');
+  CAN.endPacket();
+
+  Serial.println("done");
+
+  delay(1000);
+}
